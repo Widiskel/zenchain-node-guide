665908e3128281d7bfbbd2c602f36e293f98d601
widiskel <waputra16@gmail.com>
2024-11-10 23:26:05 +0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,34 @@
+
+## BlockMesh Network Node
+
+![Blockmesh](assets/img1.png)
+
+BlockMesh Network allows you to generate passive income from your excessive bandwidth
+
+BlockMesh Network: an open-source, secure network for ethical AI oversight with passive income opportunities.
+
+Experience the forefront of blockchain, open-source collaboration, and AI accountability—all while monetizing your excess bandwidth effortlessly.
+
+
+## Blockmesh CLI Node Setup Guide
+Run this to setup your Dria Node
+```
+[ -f setup.sh ] && rm setup.sh; wget https://raw.githubusercontent.com/Widiskel/blockmesh-node-setup/refs/heads/master/setup.sh && sudo chmod +x setup.sh && ./setup.sh || echo "Error: setup.sh could not be downloaded."
+```
+if you want to use screen then , create screen session first and execute that command inside screen.
+
+
+## Blockmesh Browser Extension
+
+You can also install blockmesh node extension to run blockmesh on your browser
+
+Link : https://chromewebstore.google.com/detail/blockmesh-network/obfhoiefijlolgdmphcekifedagnkfjp
+
+## SUPPORT
+
+Want to support me for creating another node setup ?
+**star** my repo or buy me a coffee on
+
+EVM : `0x1f0ea6e0b3590e1ab6c12ea0a24d3d0d9bf7707d`
+
+SOLANA : `3tE3Hs7P2wuRyVxyMD7JSf8JTAmEekdNsQWqAnayE1CN`
---
+++ setup.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+NODENAME="blockmesh"
+NODEPATH=""
+GREENCOLOR="\e[32m"
+DEFAULTCOLOR="\e[0m"
+
+setup() {
+    curl -s https://raw.githubusercontent.com/Widiskel/Widiskel/refs/heads/main/show_logo.sh | bash
+    sleep 3
+
+    echo "Updating & Upgrading Packages..."
+    sudo apt update -y && sudo apt upgrade -y
+
+    cd ~
+    if [ -d "node" ]; then
+        echo "The 'node' directory already exists."
+    else
+        mkdir node
+        echo "Created the 'node' directory."
+    fi
+    cd node
+
+    if [ -d "$NODENAME" ]; then
+        echo "The '$NODENAME' directory already exists."
+    else
+        mkdir $NODENAME
+        echo "Created the '$NODENAME' directory."
+    fi
+    cd $NODENAME
+}
+
+installRequirements(){
+    echo "Installing $NODENAME Compute Node"
+    ARCH=$(uname -m)
+
+    if [ "$ARCH" == "arm64" ]; then
+        echo "Architecture is arm64, downloading arm64 version."
+        curl -L -o $NODENAME-cli.tar.gz https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.352/blockmesh-cli-aarch64-unknown-linux-gnu.tar.gz
+        NODEPATH="~/node/$NODENAME/target/aarch64-unknown-linux-gnu/release"
+    elif [ "$ARCH" == "x86_64" ]; then
+        echo "Architecture is x86_64, downloading amd64 version."
+        curl -L -o $NODENAME-cli.tar.gz https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.352/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz
+        NODEPATH="~/node/$NODENAME/target/x86_64-unknown-linux-gnu/release"
+    else
+        echo "Unknown architecture: $ARCH. Exiting."
+        exit 1
+    fi
+    tar -xvzf "$NODENAME-cli.tar.gz"
+    cd $NODEPATH
+    echo "$NODENAME CLI Node Installed"
+}
+
+finish() {
+    if ! [ -f help.txt ]; then
+        {
+            echo "Setup Complete"
+            echo "Your $NODENAME path is on $NODEPATH"
+            echo ""
+            echo "Follow this guide to start your node:"
+            echo "To start Your Node run $NODEPATH/$NODENAME-cli --email YOUREMAIL --password YOURPASSWORD"
+        } > help.txt
+    fi
+    cat help.txt
+}
+
+
+run() {
+    read -p "Do you want to run it? (y/n): " response
+    if [[ $response == "y" ]]; then
+        echo -e "\nEnter your credentials:"
+        read -p "Email: " EMAIL
+        read -s -p "Password: " PASSWORD
+        $NODENAME-cli --email "$EMAIL" --password "$PASSWORD"
+    else
+        echo "LFG"
+    fi
+}
+
+
+setup
+installRequirements
+finish
+run
